22130a5f4b4db178be0425234e922abd4583b70a
Matt <59522800+mcket747econ@users.noreply.github.com>
2022-09-14 00:17:33 -0500

    Final Julia Parallelization Files

---
+++ f22stochasticcomputeparallel2.jl
@@ -0,0 +1,28 @@
+#import the libraries we want
+using Distributed
+addprocs(2)
+@everywhere using Pkg;Pkg.add(["Plots","Parameters"])
+@everywhere using Parameters, Plots
+@everywhere include("f22stochasticmodelparallel.jl") #import the functions that solve our growth model
+
+@everywhere @time prim, res = Initialize() #initialize primitive and results structs
+@everywhere @time Solve_model(prim, res) #solve the model!
+@everywhere @unpack val_func, pol_func = res
+@everywhere @unpack k_grid = prim
+
+##############Make plots
+#value function
+Plots.plot(k_grid, val_func, title="Value Function", label = ["Good Shock" "Bad Shock"])
+Plots.savefig("C:/Users/mcket/OneDrive/Documents/Fall 2022/ECON899-Computational/Problem Set 1/02Growthzipjulia/02_Value_Functions.png")
+
+#policy functions
+Plots.plot(k_grid, pol_func, title="Policy Functions", label = ["Good Shock" "Bad Shock"])
+Plots.savefig("C:/Users/mcket/OneDrive/Documents/Fall 2022/ECON899-Computational/Problem Set 1/02Growthzipjulia/02_policy_Functions.png")
+
+#changes in policy function
+pol_func_δ = copy(pol_func).-k_grid
+Plots.plot(k_grid, pol_func_δ, title="Policy Functions Changes", label = ["Good Shock" "Bad Shock"])
+Plots.savefig("C:/Users/mcket/OneDrive/Documents/Fall 2022/ECON899-Computational/Problem Set 1/02Growthzipjulia/02_polcy_changes.png")
+
+println("All done!")
+################################
