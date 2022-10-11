9488c564df68e4a076822bc47b650b79360da773
jacobbills <43051895+jacobbills@users.noreply.github.com>
2022-10-10 23:57:10 -0500

    edits
    Improved the capital path, created a sorter, defined an autroregression. Need to figure out what is x and y but after that all of the inner loop in the main solver should be ready. Figuring out the outer loop with a changing I is much more difficult

--- PS5/PS5_model.jl
+++ PS5/PS5_model.jl
@@ -6,18 +6,22 @@
 #To Do:  
 ##Full model solver
 ###Establish structutre
-###Put in precomposed functions
+###Put in precomposed functions (mostly done)
 ###Figure out how to do the goodness of fit. Seems that we might want an outer while loop. 
+###Make sure new functions have proper arguments
 
 ##Write SimulateCapitalPath
-###Ultimate goals, get average capital of each z, return a 1xT-1 vector
-###Do one column at a time
-###Burn Kappa before returning 
+###This needs to be run now, but should work fine? 
+###Make sure all dimensions are correct 
+###Add in another column to Kappa so I track K today vs K tomorrow right? For regression 
+
 
 ## write EstimateRegression
-###Make sure there are two equations, one for each state of the world
-###Will need to sort data across states
 ###Returns 4 estimated coefficients 
+### Need to determine what X and Y are. I think kappa is actually Y but it might be both? MOST IMPORTANT THING TO FIGURE OUT!
+###Regression function needs review 
+
+## Create a compiler file to run all these functions 
 
 
 
@@ -34,35 +38,88 @@ function SimulateCapitalPath(R::Results, P::Params, Epsilon,Z)
     @unpack pf_k = R
     @unpack N, T, burn = P
 
-    kappa::Array{Float64,1} = zeros(T-1) #since the first one is Kbar2
-    V::Array{Float64,2} = zeros{T,N} #Storing. Julia indexes columns first, right? 
+    kappa::Array{Float64,2} = zeros(T-1,2) #since the first one is Kbar2
+    V::Array{Float64,2} = zeros{N,T} #Storing the panel. I hate that we have to do this column by column but rows come first 
     
     #do initial column seperately
     for n in 1:N
-        V[1,n] = pf_k[11.55,Epsilon[t,n],11.55,Z[t]] #not sure what z_t in the notes refers to, 11.55 is a hardfix, should replace with steady state later 
+        V[n,1] = pf_k[11.55,Epsilon[n,1],11.55,Z[1]] #11.55 is a hardfix, should replace with steady state later 
     end
-    kappa[1] = sum(V[1,:])/N
+    kappa[1,1] = sum(V[:,1])/N
+    kappa[1,2] = Z[1]
     
     for t in 2:T
         for n in 1:N
-            V[t,n] = pf_k[V[t-1,n],Epsilon[t,n],kappa[t-1],Z[t]] #not sure what z_t in the notes refers to 
+            V[n,t] = pf_k[V[n,t-1],Epsilon[n,t],kappa[t-1],Z[t]] #need to flip my epsilon I think 
         end
-        kappa[t] = sum(V[t,:])/N
+        kappa[t,1] = sum(V[t,:])/N
+        kappa[t,2] = Z[t] #for the sort lateron
     end
 
-    #burn the first 1000 columns 
-    kappa_b::Array{Float64,1} = kappa[burn+1:T]
-    V_b::Array{Float64,2} = V[burn+1:T,:]
+    ##I might want a third column in kappa for either the previous or next average K, to act as y since I need that before I sort. 
+
+    #burn the first 1000 columns/rows (kappa is weird)
+    kappa_b::Array{Float64,2} = kappa[burn+1:T,:]
+    V_b::Array{Float64,2} = V[:,burn+1:T]
 
     return kappa_b, V_b  
 end
 
+function state_sort(array) #figure this might be worth doing, will need to check dimensions
+    n_b = length(array[:,1])
+    good::Array{Float64,1} = [0]
+    bad::Array{Float64,1} = [0]
 
-function EstimateRegression()
+    for t in 1:n_b #there must be a less janky way to do this but Julia does not like conditional deletes
+        if array[t,2] = 1
+            good = append!(good,array[t,1])
+        else
+            bad = append!(bad,array[t,1])
+        end
+    end
+    l_g = length(good)
+    l_b = length(bad)
+    good = good[2:l_g] #remove the first line
+    bad = bad[2:l_b]
+
+   ##Might need to return a Nx2 matrix instead, in which case append won't work, will need to make a state_y array instead
+    return good, bad 
+end 
+
+function log_autoreg(x,y,cons::Bool = true) #this one needs checking, especially how I get y 
+    if cons == true
+        n_x = length(x)
+        lx = log.(x)
+        xi = hcat(ones(n_x),lx) #create a new matrix with a column of ones, removes the last row because auto regressive
+        xit = transpose(xi)
+        coefs = (xit*xi)^(-1)*(xit*y) #linear regression 
+        intercept = coefs[1]
+        coef = coefs[2]
+        error = y - xi*coefs
+        y_m = y-ones(n_x)*(ones(n_x)'*ones(n_x))^(-1)*(ones(n_x)'*y)
+        R2 = (transpose(error)*error)/(transpose(y_m)*y_m)
+    return intercept, coef, R2
+    else
+        lx = log.(x) 
+        xit = transpose(xi)
+        coef = (xit*xi)^(-1)*(xit*y)
+        error = y - xi*coef
+        y_m = y-ones(n_x)*(ones(n_x)'*ones(n_x))^(-1)*(ones(n_x)'*y)
+        R2 = (transpose(error)*error)/(transpose(y_m)*y_m)
+    return coef, R2
+    end
+end
+
+
+function EstimateRegression(kappa,epsilon,cons::Bool = true )
     #first do a sort based on z
+    kappa_a, kappa_b = state_sort(kappa) #a is for good state
 
-    #Next run regressions, don't forget column vector of ones (or find a package to do this)
+    #Might need to create a Y here or I can make it in the log_autoreg fn
 
+    #Next run regressions
+    ahat0, ahat1, R2[1] = log_autoreg(kappa_a,epsilon,cons) #not sure what the right Y is, it's not simply epsilon, it might be Kappa again
+    ahat0, ahat1, R2[2] = log_autoreg(kappa_b,epsilon,cons) #not sure what the right Y is
 
     return ahat0, ahat1, bhat0, bhat1, R2
 end 
@@ -81,18 +138,18 @@ end
 
 function Solve_KS(P::Params, G::Grids, S::Shocks, R::Results, lambda::Float64=0.5, tol_up = 1e-3, m_gof::Float64 =1-1e-2, kill = 10000)
    #First step is to draw shocks
-    Z , Epsilon = DrawShocks(S,P.N,P.T) #use the formula from the helpful functions to get our shocks
+    Epsilon, Z = DrawShocks(S,P.N,P.T) #use the formula from the helpful functions to get our shocks
     #Second step is to set initial a0,b0,a1,b1
 
     R.a0, R.b0, R.a1, R.b1 = initialize_coef(R)
 
     stop = 0
     count = 0
-    while stop = 0 && R.R2 < m_gof ##wrong way to do the goodness of fit loop 
+    while stop = 0 && minimum(R.R2) < m_gof ##wrong way to do the goodness of fit loop 
         count +=1
         R.pf_k, R.pf_V = Bellman(P,G,S,R) #note this returns K' first, different than how we normally do it
 
-        kappa, V_b = SimulateCapitalPath(R,P,Epsilon,Z) #Still need to write this 
+        kappa, V_b = SimulateCapitalPath(R,P,Epsilon,Z) #I think this is all correct now 
         R.ahat0, R.ahat1, R.bhat0, R.bhat1, R.R2 = EstimateRegression(kappa) #Still need to write this 
 
         if count < kill && (abs(R.ahat0-R.a0)+abs(R.ahat1-R.a1)+abs(R.bhat0-R.b0)+abs(R.bhat1-R.b1)) > tol_up
