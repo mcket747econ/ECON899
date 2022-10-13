8739bf4fb5139bd333866ee164b4f46e879ace85
Matt <59522800+mcket747econ@users.noreply.github.com>
2022-10-12 22:28:42 -0500

    Update ps4_compute_f.jl

--- PS4/ps4_compute_f.jl
+++ PS4/ps4_compute_f.jl
@@ -4,22 +4,22 @@ cd("C:/Users/mcket/OneDrive/Documents/Fall 2022/ECON899-Computational/899Code/89
 include("ps_4_code_f.jl")
 prim, res = Initialize()
 
-val_func_0,pol_func_0,lab_func_0, K_0, L_0, r_0, w_0, b_0, F_0 =  Run_all(prim,res, .01, .3)
+val_func_0,pol_func_0,lab_func_0, K_0, L_0, r_0, w_0, b_0, F_0 =  Run_all(prim,res, .01, .3) ##Run code from pset3 to find initital steady state values with social security
 prim_2 = @set prim.theta = 0
-val_func_n, pol_func_n,lab_func_n, K_n, L_n, r_n, w_n, b_n, F_n = Run_all(prim_2,res, .01, .3)
+val_func_n, pol_func_n,lab_func_n, K_n, L_n, r_n, w_n, b_n, F_n = Run_all(prim_2,res, .01, .3) ##Running code from pset3 to find steady state without social security
 
 
-mutable struct new_primitives
+mutable struct new_primitives  ##Initializing new mutable structure
     #T_delta::Float64
     #l_delta::Float64
-    K_t::Array{Float64,1}
-    K_potential::Array{Float64,1}
-    L_t::Array{Float64,1}
+    K_t::Array{Float64,1}      ##Transition values of aggregate capital
+    K_potential::Array{Float64,1} #Initializing array that will be used later
+    L_t::Array{Float64,1} ##aggregate labor transition array
     # val_func_n::Array{Float64,1}
     sav_func_t::Array{Float64,4}
-    val_func_t::Array{Float64,4}
-    lab_func_t::Array{Float64,4}
-    pol_func_t::Array{Float64,4}
+    val_func_t::Array{Float64,4}#value function transition
+    lab_func_t::Array{Float64,4} #labor function transition
+    pol_func_t::Array{Float64,4} #policy function transition
     F_t::Array{Float64,4}
     #T::Int64
     #T_array::Array{Int64,1}
@@ -28,34 +28,34 @@ mutable struct new_primitives
     w_t::Array{Float64, 1}                                                    # transition path of wage
     r_t::Array{Float64, 1}                                                    # transition path of interest rate
     b_t::Array{Float64, 1}
-    theta::Array{Float64}
+    theta::Array{Float64} #array of capital tax values
 end
 
 
-function Initialize2(T,t_iter)
+function Initialize2(T,t_iter)   ###Initialize our main structures
     #T = 30
     #t_iter = 1
     #T_array = collect(range(1,length=30,stop=T))
     #T_delta = (K_n - K_0 )/prim.T
     #l_delta = (L_n - L_0)/prim.T
-    K_t = collect(range(K_0, stop =K_n, length = T))
+    K_t = collect(range(K_0, stop =K_n, length = T))  #initial linear guess of capital transition path
     K_potential = zeros(T)
     L_t = collect(range(L_0, stop = L_n, length = T))
     # K_t_init = collect(range(K_0, stop =K_n, length = T))
     # L_t_init = collect(range(L_0, stop = L_n, length = T))
-    val_func_t = zeros(prim.na,prim.nz,prim.N,T)
+    val_func_t = zeros(prim.na,prim.nz,prim.N,T) ###initialize various functions as
     sav_func_t = zeros(prim.na,prim.nz,prim.N,T)
     lab_func_t = zeros(prim.na,prim.nz,prim.N,T)
     pol_func_t = zeros(prim.na,prim.nz,prim.N,T)
-    F_t = zeros(prim.na,prim.nz,prim.N,T)
+    F_t = zeros(prim.na,prim.nz,prim.N,T) #Initialize distribution function
     F_t[:,:,:,1] = F_0
     pop_normal = ones(66)
     Assets = zeros(prim.na,prim.nz,prim.N,T)
-    r_t = (prim.alpha.*(L_t.^(1-prim.alpha)))./(K_t.^(1-prim.alpha)).-prim.delta
+    r_t = (prim.alpha.*(L_t.^(1-prim.alpha)))./(K_t.^(1-prim.alpha)).-prim.delta  #Initialize underlying functions that will allow for adjustment of w,r
     w_t = ((1-prim.alpha).*(K_t.^(prim.alpha)))./(L_t.^(prim.alpha))
     b_t = (prim.theta.*w_t.*L_t)./sum(F_t[:,:,prim.age_retire:prim.N,1])
     theta = [repeat([0.11], t_iter); repeat([0.0], T-t_iter)]
-    res2 = new_primitives(K_t, K_potential, L_t,sav_func_t,val_func_t,lab_func_t, pol_func_t,F_t,pop_normal,Assets,w_t,r_t,b_t,theta)
+    res2 = new_primitives(K_t, K_potential, L_t,sav_func_t,val_func_t,lab_func_t, pol_func_t,F_t,pop_normal,Assets,w_t,r_t,b_t,theta) ##Places various objects under res2.
     res2
 
 
@@ -65,7 +65,9 @@ end
 
 
 
-function bellman_t(prim::Primitives,res::Results,res2::new_primitives,T)  #Function to iterate backwards over ages to determine labor supply, asset holdings, as a function of producitivity, and age and future period asset holding
+function bellman_t(prim::Primitives,res::Results,res2::new_primitives,T)
+     #Function to iterate backwards over ages to determine labor supply, asset holdings, as a function of producitivity, and age and future period asset holding
+    #Now we have augmented the function with the transition periods T
     @unpack age_retire, gamma, sigma, z_prod, birth_distribution = prim
     @unpack markov, alpha, delta, beta, N, Assets, na, nz, age_ef, produc = prim
     @unpack w_t, r_t, b_t,theta = res2
@@ -79,37 +81,16 @@ function bellman_t(prim::Primitives,res::Results,res2::new_primitives,T)  #Funct
     val_func_t = zeros(na, nz, N, T)
     pol_func_t = zeros(na, nz, N, T)
     lab_func_t= zeros(na, nz, N, T)
-    val_func_t[:, :, :, T] = val_func_0
+    val_func_t[:, :, :, T] = val_func_0 ##Setting initial function values equal to initial steady state
     pol_func_t[:, :, :, T] = pol_func_0
     lab_func_t[:, :, :, T] = lab_func_0
-    for t = (T-1):-1:1
-        for a_index = 1:na, z_index= 1:nz
+    for t = (T-1):-1:1  ##For all transition time periods
+        for a_index = 1:na, z_index= 1:nz ##Over all asset grid values and all states nz
             c = (1+r_t[t])*Assets[a_index] + b_t[t]
-            val_func_t[a_index,z_index, N,t] = utility(prim,res,c,0,N)
+            val_func_t[a_index,z_index, N,t] = utility(prim,res,c,0,N) ##Retired aged group
         end
-        # for age_index = 66:-1:1
-        #     println("Age: ", age_index)
-            #if age_index == 66
-             #Iterate over potential asset holdings
 
-                #for a_index = 1:na, z_index=1:nz
-                # if age_index == 66   ##The problem for those in the last period of life
-                #     val_func_t[a_index,z_index, age_index,t] = -Inf #Set a very low initial guess for value function
-                #     budget::Float64 = (1+r_t[t])*Assets[a_index] + b_t[t] #
-                #     c::Float64 = budget
-                #     l::Float64 = 0
-                #     val::Float64 = utility(prim, res,c,l,age_index) #Our value function in the last period of life involves no saving
-                #     #val =  (c^((1-sigma)*gamma))/(1-sigma)
-                #     if val > val_func_t[a_index,z_index, age_index,t]   ##Check if our value function is greater than initital guess
-                #         val_func_t[a_index,z_index,age_index,t] = val #update value of this value function
-                #         pol_func_t[a_index,:,age_index,t] .= zero(UInt32) #We set both labor and saving to zero in this period
-                #         lab_func_t[a_index,:,age_index,t] .= zero(UInt32)
-                #          #I am addi
-                #         ##Add the value function over all asset levels for those in the final period of life.
-                #         # res.v_final[:,:,66] .= v_0
-                #     end
-
-        for age_index = N-1:-1:age_retire
+        for age_index = N-1:-1:age_retire ##Retired individuals
             println("Age: ", age_index)
             #elseif age_index >= 46 && age_index < 66  ##The problem for retired agents
             for a_index = 1:na, z_index=1:nz
@@ -127,7 +108,6 @@ function bellman_t(prim::Primitives,res::Results,res2::new_primitives,T)  #Funct
                     val_func_cand= val
                     pol_func_t[a_index,z_index,age_index,t] = Assets[ap_index] #Update our saving
                     lab_func_t[a_index,z_index,age_index,t] = l #We dont work
-                     ##Add the value function over all asset levels for those in the final period of life.
 
                 end
                 val_func_t[a_index,z_index, age_index,t] = val_func_cand
@@ -169,7 +149,7 @@ function bellman_t(prim::Primitives,res::Results,res2::new_primitives,T)  #Funct
 end
 
 
-function Fdist_t(prim::Primitives,res2::new_primitives,res::Results,F_0,F_n,T)
+function Fdist_t(prim::Primitives,res2::new_primitives,res::Results,F_0,F_n,T) ##function to get our distribution transition function
 
 
     @unpack na,nz,markov,N,Assets,n = prim
@@ -178,19 +158,19 @@ function Fdist_t(prim::Primitives,res2::new_primitives,res::Results,F_0,F_n,T)
     F_next= zeros(na,nz,N,T)
     F_next[:,:,:,1] = F_0
     for t_index = 2:T
-        F_next[1, :,1, t_index] = F_next[1, :, 1, 1]
+        F_next[1, :,1, t_index] = F_next[1, :, 1, 1]  ##Must initialize a youngest age goup for all transition periods or else our population mass will leak.
     end
-    for t in 1:T-1
+    for t in 1:T-1 ##fror all time periods
         for age_index = 1:N-1
             for a_index in 1:na
                 ap_index = 0
                 for z_index in 1:nz
                     if F_next[a_index,z_index,age_index,t] > 0
                     #if F_next[a_index,z_index,age_index,T] > 0
-                        ap = res2.pol_func_t[a_index,z_index,age_index,t]
-                        ap_index = 1* argmin(abs.(ap.-Assets))
+                        ap = res2.pol_func_t[a_index,z_index,age_index,t] ##Working backwards to obtain pol func values for particular a,z, and age values in particular transition values t
+                        ap_index = 1* argmin(abs.(ap.-Assets)) ##figure out where pol_func value lies on the asset grid
                         for zp_index in 1:nz
-                            F_next[ap_index,zp_index,age_index+1,t+1] += markov[z_index,zp_index]*F_next[a_index,z_index,age_index,t]./(1+n)
+                            F_next[ap_index,zp_index,age_index+1,t+1] += markov[z_index,zp_index]*F_next[a_index,z_index,age_index,t]./(1+n) ##Assign F distribution to that asset value
                         #end
                         end
                     end
@@ -209,7 +189,7 @@ function Fdist_t(prim::Primitives,res2::new_primitives,res::Results,F_0,F_n,T)
 end
 
 # res2.F_t = Fdist_t(prim,res2,res,F_0,F_n)
-function labor_t(prim::Primitives,res::Results,res2::new_primitives,age_index,a_index,ap_index,z_index,theta,t)  ##Optimal Labor Supply function
+function labor_t(prim::Primitives,res::Results,res2::new_primitives,age_index,a_index,ap_index,z_index,theta,t)  ##Labor Function over transition periods
     @unpack gamma, produc,Assets = prim
     @unpack w_t,r_t,b_t = res2
     l  = (gamma*(1-theta[t])*produc[age_index,z_index]*w_t[t]-(1-gamma)*((1+r_t[t])*Assets[a_index]-Assets[ap_index]))/((1-theta[t])*w_t[t]*produc[age_index, z_index]) ##Optimal Endogenous labor supply, including depreciation
@@ -221,7 +201,7 @@ function labor_t(prim::Primitives,res::Results,res2::new_primitives,age_index,a_
     l
 end
 
-function agg_K_t(prim::Primitives,res2::new_primitives,res::Results,K_0,T)
+function agg_K_t(prim::Primitives,res2::new_primitives,res::Results,K_0,T)   ##Aggregate capital values over k periods
     @unpack sav_func_t,F_t =res2
     @unpack N, na, nz, Assets = prim
 
@@ -257,35 +237,12 @@ end
 #
 # end
 # A = new_equilibrium()
-function new_equilibrium(prim::Primitives,res2::new_primitives,res::Results,K_0)
-    @unpack T,sav_func_t,F_t =res2
-    @unpack N, na, nz, Assets = prim
 
-   K_potential = zeros(T)
-   K_potential[1] = K_0
-   for t in 1:T-1
-        println("We're on Transition period: ",t)
-        for age_index in 1:N
-            # println("We're on Age: ",age_index)
-            for a_index in 1:na
-                # println("We're on Asset: ",a_index)
-                for z_index in 1:nz
-                    K_potential[t+1] += Assets[a_index].*F_t[a_index,z_index,age_index,t+1]
-                    println(Assets[a_index].*F_t[a_index,z_index,age_index,t+1])
-
-                end
-                # sav_func_t[a_index,z_index,age_index,t]
-            end
-        end
-    end
-    K_potential
-
-end
 
 # res2.K_potential = new_equilibrium(prim,res2,res,K_0)
 # res2.K_potential = agg_L_t(prim,res2,res,K_0)
 
-function agg_L_t(prim::Primitives,res2::new_primitives,res::Results,L_0,T)
+function agg_L_t(prim::Primitives,res2::new_primitives,res::Results,L_0,T)  ##Aggrefate labor transition path
     @unpack sav_func_t,F_t,lab_func_t =res2
     @unpack N, na, nz, Assets, produc = prim
 
@@ -342,7 +299,7 @@ end
 
 
 ###Now We Compute the Transition Path
-function overall_solve(prim::Primitives,res::Results,t,T)
+function overall_solve(prim::Primitives,res::Results,t,T) #Function to solve all code when policy change occurs in t=1
         @unpack alpha, delta, age_retire, N, na,nz = prim
         T_delta = 20
         # T = 30
@@ -355,21 +312,21 @@ function overall_solve(prim::Primitives,res::Results,t,T)
                 i += 1
                 println("***********************************")
                 println("Trial #", i)
-                res2.val_func_t, res2.pol_func_t, res2.lab_func_t = bellman_t(prim, res, res2, T)
-                res2.F_t = Fdist_t(prim, res2, res, F_0,F_n,T)
-                K_t1 = agg_K_t(prim,res2,res,K_0,T)
+                res2.val_func_t, res2.pol_func_t, res2.lab_func_t = bellman_t(prim, res, res2, T) #Solve for value, policy and labor transition functions
+                res2.F_t = Fdist_t(prim, res2, res, F_0,F_n,T)##get F transition function
+                K_t1 = agg_K_t(prim,res2,res,K_0,T) ##Aggregate labor and capital
                 L_t1 = agg_L_t(prim,res2,res,L_0,T)
-                display(plot([res2.K_t K_t1 repeat([K_0], T) repeat([K_n], T)],
+                display(plot([res2.K_t K_t1 repeat([K_0], T) repeat([K_n], T)], ##Displaying capital path over the course of trials
                          label = ["K Guess" "K Path" "Stationary K w/ SS" "Stationary K w/o SS"],
                          title = "Capital Transition Path", legend = :bottomright))
-                diff = maximum(abs.(res2.K_t .- K_t1)./K_t1) + maximum(abs.(res2.L_t .- L_t1)./L_t1)
+                diff = maximum(abs.(res2.K_t .- K_t1)./K_t1) + maximum(abs.(res2.L_t .- L_t1)./L_t1) #Check gap between guess and second steady state value
                 # @printf("Difference: %0.3f.", float(diff))
                 println("")
                 if diff > tol
                     KL_update2(prim,res2,K_t1,L_t1,lambda,F_0,T)
                     println("Paths Adjusted")
                 else
-                    println("Paths Converged")
+                    println("Paths Converged") ##Once paths have converged print the necessary graphs
                     display(plot([res2.K_t K_t1 repeat([K_0], T) repeat([K_n], T)],
                              label = ["K Guess" "K Path" "Stationary K w/ SS" "Stationary K w/o SS"],
                              title = "Capital Transition Path", legend = :bottomright))
@@ -413,10 +370,10 @@ function overall_solve(prim::Primitives,res::Results,t,T)
         res2, T
 end
 
-elapse = @elapsed res2, T = overall_solve(prim, res,1, 30)
+elapse = @elapsed res2, T = overall_solve(prim, res,1, 30) ##Run function
 
 
-function EV(prim::Primitives, res::Results,res2::new_primitives,lam::Float64)
+function EV(prim::Primitives, res::Results,res2::new_primitives,lam::Float64)  #3Calculating Equivalent Variation values
     @unpack N, na, nz, alpha,sigma = prim
     ev = zeros(na, nz, N)
     evj = zeros(N)
@@ -442,7 +399,7 @@ vote_share_counterfact = sum((ev_cf.>=1).*F_0)
 diff_vote_share = vote_share_counterfact - vote_share
 
 
-function overall_solve2(prim::Primitives,res::Results,t,T)
+function overall_solve2(prim::Primitives,res::Results,t,T)  ##function to solve all code when policy occurs in t=21
         @unpack alpha, delta, age_retire, N, na,nz = prim
         T_delta = 20
         # T = 30
@@ -513,9 +470,9 @@ function overall_solve2(prim::Primitives,res::Results,t,T)
         res2, T
 end
 
-elapse = @elapsed res2_2, T_2 = overall_solve2(prim, res,20, 50)
+elapse = @elapsed res2_2, T_2 = overall_solve2(prim, res,20, 50)  ##
 
-evj2, evj_cf2, ev2, ev_cf2 = EV(prim,res,res2_2,0.42)
+evj2, evj_cf2, ev2, ev_cf2 = EV(prim,res,res2_2,0.42) ##Calculating EV values for policy occuring at t= 21
 sum((ev2.>=1).*F_0)
 
 plot(collect(20:85), [evj2[1:66], evj_cf2[1:66], repeat([1], 66)], labels = ["With Transition" "Without Transition" "EV = 1"],
@@ -526,4 +483,6 @@ println(" ")
 vote_share2 = sum((ev2.>=1).*F_0)
 vote_share_counterfact2 = sum((ev_cf2.>=1).*F_0)
 diff_vote_share2 = vote_share_counterfact2 - vote_share2
+
+evj2[60]
 
