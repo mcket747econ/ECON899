52346249f3b17c48259d4321c3f5040bd46bbdc8
Matt <59522800+mcket747econ@users.noreply.github.com>
2022-10-21 12:39:35 -0500

    Update PS5_compute.jl

--- PS5/PS5_compute.jl
+++ PS5/PS5_compute.jl
@@ -1,9 +1,10 @@
-using Parameters, Plots, Optim, Interpolations  #last two are needed for the helpful functions
+using Parameters, Plots, Optim, Interpolations, GLM, Printf, DataFrames #last two are needed for the helpful functions
 using Random, Distributions, LinearAlgebra
 
 # cd("/Users/jacobbills/Desktop/Economics/Econ 899/PS 5/")
-if occursin("Rafeh",pwd()) cd("C:/Users/Rafeh/Documents/GitHub/ECON899/PS5") end #Insert your own paths here 
-# cd("/Users/jacobbills/Desktop/Economics/Econ 899/PS 5/") #Insert your own paths here 
+if occursin("Rafeh",pwd()) cd("C:/Users/Rafeh/Documents/GitHub/ECON899/PS5") end #Insert your own paths here
+# cd("/Users/jacobbills/Desktop/Economics/Econ 899/PS 5/") #Insert your own paths here
+if occursin("mcket",pwd()) cd("C:/Users/mcket/OneDrive/Documents/Fall 2022/ECON899-Computational/899Code/899/ECON899/PS5") end #Insert your own paths here
 
 include("HelpfulFunctions_edits.jl")
 include("ps5_model.jl")
@@ -11,4 +12,5 @@ include("ps5_model.jl")
 ## Moved everything to solver
 ## Should probably remove "print" in KS_solve
 overall_solve()
+
 
