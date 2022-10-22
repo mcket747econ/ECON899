363bfed6b3caeac2429b479ba5923875e9d76b6d
Matt <59522800+mcket747econ@users.noreply.github.com>
2022-10-22 10:16:15 -0500

    Update PS5_compute.jl

--- PS5/PS5_compute.jl
+++ PS5/PS5_compute.jl
@@ -6,11 +6,12 @@ if occursin("Rafeh",pwd()) cd("C:/Users/Rafeh/Documents/GitHub/ECON899/PS5") end
 # cd("/Users/jacobbills/Desktop/Economics/Econ 899/PS 5/") #Insert your own paths here
 if occursin("mcket",pwd()) cd("C:/Users/mcket/OneDrive/Documents/Fall 2022/ECON899-Computational/899Code/899/ECON899/PS5") end #Insert your own paths here
 
-include("HelpfulFunctions_edits.jl")
-include("ps5_model.jl")
+include("helpful_functionedits_final.jl")
+include("ps5_model_final.jl")
 
 ## Moved everything to solver
 ## Should probably remove "print" in KS_solve
-overall_solve()
+P,G,S,R = initialize_overall()
+ overall_solve(P,G,S,R)
 
 if occursin("Rafeh",pwd()) cd("C:/Users/Rafeh/Documents/GitHub/ECON899/PS5") end #Insert your own paths here
