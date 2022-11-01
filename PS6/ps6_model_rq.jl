8691fdf2e9691e4a5fc4810e0088502bff3b48a5
Rafeh Qureshi <40925461+rafehq@users.noreply.github.com>
2022-11-01 03:42:00 -0500

    Add files via upload

--- PS6/ps6_model_rq.jl
+++ PS6/ps6_model_rq.jl
@@ -2,17 +2,15 @@
 using Optim, Plots, Parameters, Distributions, Random, DataFrames
 
 @with_kw struct Params
-
-    beta::Float64=0.8;
-    A::Float64=0.6;
+    β::Float64=0.8;
+    A::Float64=0.6;     ##Heejin used as 1/200 ?
     γE::Float64=0.5772156649;
 
-    theta::Float64=0.64;
+    θ::Float64=0.64;
     cf::Float64=10;
     ce::Float64=5;
     s_grid::Array{Float64,1} = [3.98e-4, 3.58, 6.82, 12.18, 18.79]
     n_s::Int64 = length(s_grid)
-    emp_levels::Array{Float64,1} = [1.3e-9, 10, 60, 300, 1000]
     F_transition::Array{Float64,2} = [0.6598 0.2600 0.0416 0.0331 0.0055;
                                       0.1997 0.7201 0.0420 0.0326 0.0056;
                                       0.2000 0.2000 0.5555 0.0344 0.0101;
@@ -20,13 +18,12 @@ using Optim, Plots, Parameters, Distributions, Random, DataFrames
                                       0.2000 0.2000 0.2500 0.3400 0.0100]
 
     v_s_entrant::Array{Float64,1} =[0.37,0.4631,0.1102,0.0504,0.0063]
+    
+    emp_levels::Array{Float64,1} = [1.3e-9, 10, 60, 300, 1000]
     tau::Float64=0;
-
     p_star::Float64=0.738;
     w::Float64=1;
     lambda::Float64 =.99;
-
-
     rho::Float64=0.93;
     sigma_logz::Float64=sqrt(0.53);
     sigma_epsilon::Float64=sqrt((1-rho)*((sigma_logz)^2));
@@ -61,36 +58,35 @@ function Initialize()
     return P, R
 end
 P,R = Initialize()
-function n_star(s,theta,p_star,s_grid)
-    n = ((1/theta)*p_star*s_grid[s])^((1/theta)-1)
+function n_star(s,θ,p_star)
+    n = ((1/θ)*p_star*s)^((1/θ)-1)
     return n
 end
-function profit(P::Params,R::Results,i)
+function profit(P::Params,R::Results,is)
     @unpack p_star,pf_n_func, val_func = R
-    @unpack s_grid, cf, theta, n_s = P
-    prof_1 = p_star*s_grid[i]*n_star(i,theta,p_star,s_grid)^(theta) - n_star(i,theta,p_star,s_grid) - p_star*cf
-
+    @unpack s_grid, cf, θ, n_s = P
+    s = s_grid[is]
+    nopt=n_star(s,θ,p_star)
+    prof_1 = p_star*s*nopt^(θ) - nopt - p_star*cf
     return prof_1
 end
 prof = profit(P,R,1)
 
-function Utility(p::Float64,P::Params,R::Results,α::Float64=1)  ##utility
+function Utility(P::Params,R::Results,α::Float64=1,val_func_0,val_func_1)  ##utility
     @unpack γE= P
     @unpack val_func= R
-    c= 10
-    for is in 0:n_s
-        γE/α + (1/α)*(c+log(exp(α*val_func[is]-c)))
-    end
+    c= (val_func_0+val_func_1)/2
+    γE/α + (1/α)*(c+log(exp(α*val_func_0-c) + exp(α*val_func_1-c))), exp(α * (val_func_1 -c))/(exp(α * (val_func_0 - c)) + exp(α * (val_func_1 - c)))
 end
 
 function VFI(P::Params,R::Results)
-    @unpack n_s, F_transition, beta= P
+    @unpack n_s, F_transition, β= P
     val_func = zeros(n_s)
     pf_entry_x = zeros(n_s)
     val_func_in = zeros(n_s)
     val_func_out = zeros(n_s)
     for i = 1:n_s
-        val_func_in[i] = profit(P,R,i) + beta*sum(val_func[:].*F_transition[i,:])
+        val_func_in[i] = profit(P,R,i) + β*sum(val_func[:].*F_transition[i,:])
         val_func_out[i] += profit(P,R,i)
         if val_func_in[i] > val_func_out[i]
             val_func[i] = val_func_in[i]
@@ -103,11 +99,43 @@ function VFI(P::Params,R::Results)
     return val_func, pf_entry_x
         #Then we want to solve the static labor problem
 end
+
+function VFI_shocks(P::Params,R::Results)
+    @unpack n_s, F_transition, β= P
+    @unpack pf_prof = R
+    val_func = zeros(n_s)
+    pf_entry_x = zeros(n_s)
+    val_func_1 = zeros(n_s)
+    val_func_0 = zeros(n_s)
+    for i = 1:n_s
+        val_func_0[i] = profit(P,R,i) + β*sum(val_func[:].*F_transition[i,:])
+        val_func_1[i] += profit(P,R,i)
+        val_func[i],pf_entry_x[i] = Utility(P,R,α,val_func_0,val_func_1)
+    end
+    return val_func, pf_entry_x
+        #Then we want to solve the static labor problem
+end
+
+function VFI_Star(prim::Primitives, res::Results)
+    @unpack ns, F, v_s_entrant = prim
+    @unpack pf_entry_x, val_func, mu, M = res
+
+    muprime = zeros(ns)
+    for is = 1:ns
+        for isp = 1:ns
+            muprime[sp_index] += (1 - pf_entry_x[is]) * mu[is] * 
+            F[is, isp] + (1 - pf_entry_x[is]) *
+            F[is, isp] * M * v_s_entrant[is]
+        end
+    end
+    muprime
+end
+
 # function VFI(P::Params,R::Results)
-#     @unpack n_s, F_transition, beta= P
+#     @unpack n_s, F_transition, β= P
 #     @unpack val_func, pf_entry_x = R
 #     for is = 1:n_s
-#         wint = beta*sum(val_func[:].*F_transition[is,:])
+#         wint = β*sum(val_func[:].*F_transition[is,:])
 #         if wint > 0
 #             val_func[i] = profit(P,R,i)+wint
 #             pf_entry_x[i] = 0
@@ -120,8 +148,6 @@ end
 # end
 R.val_func, pf_entry_x = VFI(P,R)
 
-
-
 function Entval(R::Results,P::Params)
     @unpack n_s = P
     @unpack val_func =  R
@@ -131,7 +157,7 @@ function Entval(R::Results,P::Params)
     return W
 end
 
-function StatDist(R::Results,P::Primitives)
+function StatDist(R::Results,P::Params)
     @unpack pf_entry_x = R
     @unpack F_transition = P
     for is in 1:n_s
@@ -155,7 +181,7 @@ function LMC(mu,M) ##Labor market clearing
 end
 
 
-function distr(P::Primitives, R::Results, tol::Float64 =-1e3)
+function distr(P::Params, R::Results, tol::Float64 =-1e3)
     @unpack n_s = P
     @unpack pf_entry_x,M = R
     mu = ones(n_s)/n_s
@@ -170,12 +196,12 @@ function distr(P::Primitives, R::Results, tol::Float64 =-1e3)
 end
 
 
-function dist_iterate(prim::Primitives, res::Results, tol::Float64 = 1e-3)
+function dist_iterate(prim::Params, res::Results, tol::Float64 = 1e-3)
     error = 100
     n = 0
     while error>tol
         n+=1
-        mu_next = distribution(prim,res)
+        mu_next = VFI_Star(prim,res)
         error = maximum(abs.(mu_next-res.mu))#I assume this is the maximum of the array
         res.mu = mu_next
         println("Iteration number: ", n)
@@ -231,7 +257,7 @@ function solve_HR2(P::Params,R::Results,tol=1e-3)
     p0 = p_star
     convergence = 0
     while converge == 0
-        R.val_func,pf_entry_x = VFI(P,R)
+        R.val_func,R.pf_entry_x = VFI(P,R)
         W = Entval(R,P)
         if abs(W - p0*ce) > tol
             if W > p0*ce
@@ -261,10 +287,8 @@ end
 function EVS()
     u_0 = zeros(n_s)
     for i =1:n_s
-        val_func_in = profit({,R,i}) + beta *sum(u_0[:].*F_transition[i,:])
+        val_func_in = profit(P,R,i) + β *sum(u_0[:].*F_transition[i,:])
         val_funcOut = profit(P,R,i)
     end
     U_1[i] = (gamma_e/alpha_1) +(1/alpha_1)*log((e^(alpha_1*val_func_in[i])+e^(alpha_1*val_func_out[i])))
-
-
 end
