0f4085e66a0fb6a9f5777d16ae1fdc7ee67cc785
jacobbills <43051895+jacobbills@users.noreply.github.com>
2022-11-03 19:20:14 -0500

    Add files via upload

---
+++ JF 1/ps1b_compute.jl
@@ -0,0 +1,44 @@
+using StatFiles, DataFrames, Optim, LinearAlgebra
+
+cd("/Users/jacobbills/Desktop/Economics/Econ 899/JF 1/")
+include("ps1b_model.jl") #source of functions
+
+data = DataFrame(load("Mortgage_performance_data.dta"))
+
+y = data[!,20]
+y = Array(y)
+ind = ["i_large_loan", "i_medium_loan" , "rate_spread" , "i_refinance", "age_r", "cltv", "dti", "cu" , "first_mort_r", "score_0", "score_1", "i_FHA", "i_open_year2", "i_open_year3", "i_open_year4", "i_open_year5"]
+x = data[!,ind]
+insertcols!(x, 17, :const => 1) 
+x = Matrix(x)
+β = zeros(size(x,2)) #I counted sixteen variables in the problem set, plus a constant
+β[17] = -1 #if I understand the problem right
+
+
+#Question 1
+
+ll1, score1, H1 = evaluate_b(β,y,x)
+
+println("The Log-Likelihood is ", ll1)
+println("The score of the LL is: ", score1)
+println("The Hessian of the LL is: ", H1)
+
+##Question 2 (not sure how to approach this)
+
+
+#Question 3 
+@time b3, ll3= newton(β, y, x)
+
+println("The coeffcients are: ", b3)
+println("The log-likelihood is ", ll3)
+
+#Question 4
+
+fun(b) = LL(b,y,x) #define a new function for the purposes of minimizing only β
+@time opta = optimize(fun, β, BFGS()) #optimize the function using BFGS 
+b4a = Optim.minimizer(opta) #get coefficients 
+println("The BFGS coefficients are: ", b4a)
+
+@time optb = optimize(fun, β, NelderMead()) #Simplex method 
+b4b = Optim.minimizer(optb) #get coefficients 
+println("The Simplex coefficients are: ", b4b)
--- JF 1/ps1b_model.jl
+++ JF 1/ps1b_model.jl
@@ -4,23 +4,13 @@
 
 #Todo
 
-##Write Three functions (all conditional on \beta)
-
-###Log-Likelihood
-
-###Score of Log-Likelihood
-
-###Hessian (annoying but okay)
-
-##Evaluate at two different betas {-1,0}
-
 ##Solve ML problem
 
 ###Use Newton's method
 
 ##Solve ML problem again with built in optimizers. 
 
-using StatFiles, DataFrames
+using StatFiles, DataFrames, LinearAlgebra 
 
 cd("/Users/jacobbills/Desktop/Economics/Econ 899/JF 1/")
 
@@ -32,54 +22,72 @@ ind = ["i_large_loan", "i_medium_loan" , "rate_spread" , "i_refinance", "age_r",
 x = data[!,ind]
 insertcols!(x, 17, :const => 1) 
 x = Matrix(x)
-β = zeros(size(x)) #I counted sixteen variables in the problem set, plus a constant
+β = zeros(size(x,2)) #I counted sixteen variables in the problem set, plus a constant
 β[17] = -1 #if I understand the problem right
 
 ##Question 1 
 function LL(beta,y,X) #takes in an input matrix of betas, seems to work 
     N = size(y,1) #number of observations
-    li = zeros(N)
-    for i in 1:N
-        li[i] = -log(1+exp(X[i,:]'*beta)+y[i]*X[i,:]'*beta)
+    li = zeros(N) #initialize output vector
+    for i in 1:N 
+        li[i] = -log(1+exp(X[i,:]'*beta))+y[i]*X[i,:]'*beta #formula for log-Likelihood
     end
-    l = sum(li)
+    l = sum(li) #log-Likelihood is a single number 
     return l
 end 
 
-function sx(beta,X)
+function sx(beta,X) #just a formula I will use a lot 
     1/(1+exp(-X'*beta))
 end
 
 function score_LL(beta,y,X) #score is first derivatives with respect to beta
-    N,K = size(X)
-    sl = zeros(K,N)
+    N,K = size(X) #number of observations  
+    sl = zeros(K,N) #initialize output array 
     for i in 1:N
-        s = y[i]-sx(beta,X[i,:])
-        sl[:,i] = s*X[i,:]'
+        s = y[i]-sx(beta,X[i,:])  
+        sl[:,i] = s*X[i,:]' # general formula 
     end
     score::Array{Float64,1} = zeros(K)
     for k in 1:K
-        score[k] = sum(sl[k,:])
+        score[k] = sum(sl[k,:]) #ultimately, want a vector of sums
     end
     return score 
 end
 
 
 function hessian(beta,X) #hessian is second derivatives wrt beta
-    N,K = size(X)
-    Hl = zeros(K,K,N)
-    H = zeros(K,K)
+    N,K = size(X) #getting sizes 
+    Hl = zeros(K,K,N) #begin with 3 dimensions
+    H = zeros(K,K) #final result is a KxK matrix formed from the sums of each element 
     for i in 1:N
         s = sx(beta,X[i,:])
-        Hl[:,:,i] = X[i,:]*X[i,:]'.*s.*(1-s)
-        H += Hl[:,:,i]
+        Hl[:,:,i] = X[i,:]*X[i,:]'.*s.*(1-s) #most of the formula 
+        H += Hl[:,:,i] #getting rid of the number of observations dimension 
     end 
+    H = -H
     return H
 end
 
-function evaluate_b(beta,y,X)
+function evaluate_b(beta,y,X) #runs the above in one function 
     ll = LL(beta,y,X)
     score = score_LL(beta,y,X)
     H = hessian(beta,X)
     return ll, score, H 
+end
+
+function newton(beta, y, X, tol::Float64 = 1e-9, step::Float64 = .5)
+    b0 = beta 
+    flag = 0 #convergence flag
+    while flag == 0 #iterate until converged 
+        ll, score, H = evaluate_b(b0,y,X)
+        bk = b0 - H^(-1)*score
+        if norm(bk-b0) < tol
+            b0 = bk 
+            flag = 1 #update convergence flag 
+        else 
+            b0 = bk #add in a step maybe? 
+        end 
+    end
+    ll = LL(b0,y,X)
+    return b0, ll #might want the log-likelihood along with the rest of this
 end
