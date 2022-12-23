365dfc9ac91cce5e59b38d1af2c9c1d1b35001c6
Matt <59522800+mcket747econ@users.noreply.github.com>
2022-12-22 20:25:00 -0600

    Create ps4_compute.jl

---
+++ JF 4/ps4_compute.jl
@@ -0,0 +1,16 @@
+using DataFrames, Random, Parameters, Distributions, Accessors, CSV, LinearAlgebra
+include("ps4_code.jl")
+
+P,R = initialize(S)
+
+overall_iteration(P,R,S)
+R.exp_val_func,R.exp_val_vector = expected_val_func(P,R) 
+
+
+### P_Hat Using Simulate Data
+sim_phat = CCP(P,R)
+R.exp_val_func_ccp,payoff_0,payoff_1,F = vbar_ccp(P,R,S,sim_phat)
+#### Find Expected Value Function From CCP With Iteration Starting from Phat = 0.5
+
+overall_ccp_iteration(P,R)
+R.exp_val_func_ccp
